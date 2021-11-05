3c4f2691a93e3323ec51b54f513a88d17c6b20ae
Nawaf <princenifam@gmail.com>
2021-11-05 10:18:56 +0300

    Testing Started :)

--- src/HeyGuys.java
+++ src/HeyGuys.java
@@ -3,6 +3,18 @@ public class HeyGuys {
 
     public static void main(String[] args) {
         System.out.println("Hello, world.. from Nawaf :)");
+        System.out.println("Now it is your turn!");
+
+        //TODO : HADI MARZOOQ
+
+
+        //TODO : HADI AKSAIAN
+
+
+        //TODO : ALI ALHERZ
+
+
+        //TODO : for(;;;) :)
 
     }
 }
